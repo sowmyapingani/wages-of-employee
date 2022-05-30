46e7d0a6f7ba8bf92d886a2cd97f12e49ed8a215
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-05-30 19:40:57 +0530

    added:totalhours

--- employeeWages1.java
+++ employeeWages1.java
@@ -3,15 +3,19 @@ import java.util.Random;
 public class employeeWages1 {
     public static void main(String[] args) {
         System.out.println("Welcom to Employee Wage Camputation Program");
-        System.out.println("Welcom to Employee Wage Camputation Program");
+
         Random random = new Random();
+
         int wagePerHour = 20;
         int fullDayHours = 8;
         int partTimeHours = 4;
         int monthlySalary = 0;
+        int monthlyHours = 0;
         int wagePerDay = 0;
+        int days = 0;
 
-        for (int day = 1; day <= 20; day++) {
+        while (monthlyHours <= 100 && days <= 20) {
+
             int attendance = random.nextInt(3);
             wagePerDay = 0;
             switch (attendance) {
@@ -20,11 +24,15 @@ public class employeeWages1 {
                     break;
                 case 1:
                     wagePerDay = partTimeHours * wagePerHour;
+                    monthlyHours = monthlyHours + partTimeHours;
                     break;
                 default:
                     wagePerDay = wagePerHour * fullDayHours;
+                    monthlyHours = monthlyHours + fullDayHours;
             }
             monthlySalary = monthlySalary + wagePerDay;
+            System.out.println("Day:" + days + "monthlyHours:" + monthlyHours + "monthlySalary");
+            days++;
         }
     }
 }
