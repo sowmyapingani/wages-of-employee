f1a697cd24e31b48fc431b75bfde5332a166338d
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-05-30 19:32:04 +0530

    added:monthlywages

--- employeeWages1.java
+++ employeeWages1.java
@@ -1,21 +1,32 @@
 package com.bridgelabs.java;
 import java.util.Random;
 public class employeeWages1 {
-        public static void main(String[] args) {
-            System.out.println("Welcom to Employee Wage Camputation Program");
-            Random random = new Random();
-            int attendance = random.nextInt(2);
-            int wagePerHour = 20;
-            int fullDayHours = 8;
-            int wagePerDay = 8;
+    public static void main(String[] args) {
+        System.out.println("Welcom to Employee Wage Camputation Program");
+        System.out.println("Welcom to Employee Wage Camputation Program");
+        Random random = new Random();
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+        int partTimeHours = 4;
+        int monthlySalary = 0;
+        int wagePerDay = 0;
 
-            if (attendance == 1) {
-                System.out.println("Employee Present");
-                wagePerDay = wagePerHour * fullDayHours;
-            } else
-                System.out.println("Employee Absent");
-            System.out.println("Employee Wage Per Day:" + wagePerDay);
+        for (int day = 1; day <= 20; day++) {
+            int attendance = random.nextInt(3);
+            wagePerDay = 0;
+            switch (attendance) {
+                case 0:
+                    System.out.println("Employee Absent");
+                    break;
+                case 1:
+                    wagePerDay = partTimeHours * wagePerHour;
+                    break;
+                default:
+                    wagePerDay = wagePerHour * fullDayHours;
+            }
+            monthlySalary = monthlySalary + wagePerDay;
         }
     }
+}
 
 
