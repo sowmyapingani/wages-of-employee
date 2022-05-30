93c8f627238df4c5bd56b4d503262a8d1d30991d
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-05-30 07:40:24 +0530

    added:logic to check attendance

--- employeeWages1.java
+++ employeeWages1.java
@@ -1,8 +1,18 @@
 package com.bridgelabs.java;
-
+import java.util.Random;
 public class employeeWages1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
+        Random random =new Random();
+        int attendance=random.nextInt(2);
+
+        if(attendance == 1)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+
+
+
+        }
     }
-}
