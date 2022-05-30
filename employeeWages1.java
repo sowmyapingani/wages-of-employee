e93d1a4df576ae468e0091c39ad016d0cb9a6641
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-05-30 19:22:09 +0530

    added:parttime

--- employeeWages1.java
+++ employeeWages1.java
@@ -1,30 +1,21 @@
 package com.bridgelabs.java;
 import java.util.Random;
 public class employeeWages1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-                Random random  = new Random();
-                int attendance = random.nextInt(2);
-                int wagePerHour=20;
-                int fullDayHours=8;
-                int partTimeHours=4;
-                int wagePerDay=0;
+        public static void main(String[] args) {
+            System.out.println("Welcom to Employee Wage Camputation Program");
+            Random random = new Random();
+            int attendance = random.nextInt(2);
+            int wagePerHour = 20;
+            int fullDayHours = 8;
+            int wagePerDay = 8;
 
-                switch(attendance)
-                {
-                    case 0: System.out.println("Employee Absent");
-                        break;
-                    case 1: wagePerDay = partTimeHours * wagePerHour;
-                        break;
-                    default:
-                        wagePerDay =wagePerHour * fullDayHours;
-                }
-                if(attendance == 1) {
-                    System.out.println("Employee Present");
-                    wagePerDay = wagePerHour * fullDayHours;
-                }
-                else
-                    System.out.println("Employee Absent");
-                System.out.println("Employee Wage Per Day:" + wagePerDay);
-            }
+            if (attendance == 1) {
+                System.out.println("Employee Present");
+                wagePerDay = wagePerHour * fullDayHours;
+            } else
+                System.out.println("Employee Absent");
+            System.out.println("Employee Wage Per Day:" + wagePerDay);
         }
+    }
+
+
