6125a507505527acc8b2906307517c36fe6529a0
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-05-30 07:50:20 +0530

    added:logic to check Daily-wage

--- employeeWages1.java
+++ employeeWages1.java
@@ -3,16 +3,21 @@ import java.util.Random;
 public class employeeWages1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        Random random = new Random();
+        int attendance = random.nextInt(2);
 
-        Random random =new Random();
-        int attendance=random.nextInt(2);
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+        int wagePerDay = wagePerHour + fullDayHours;
 
-        if(attendance == 1)
+        if (attendance == 1) {
             System.out.println("Employee is present");
-        else
+            wagePerDay = wagePerHour * fullDayHours;
+        } else {
             System.out.println("Employee is absent");
-
+            System.out.println("Employee wage Per Day : " + wagePerDay);
 
 
         }
     }
+}
