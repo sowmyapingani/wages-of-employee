129adaa84818bc80d738da59ae25c2fe7f3732f1
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-05-30 07:26:28 +0530

    added:Employeecomputation

---
+++ employeeWages1.java
@@ -0,0 +1,8 @@
+package com.bridgelabs.java;
+
+public class employeeWages1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+    }
+}
