b0802854033b2db0846273a0ca89217a17e7f79b
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-05-30 19:09:18 +0530

    added:switchcase

--- employeeWages1.java
+++ employeeWages1.java
@@ -3,21 +3,28 @@ import java.util.Random;
 public class employeeWages1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        Random random = new Random();
-        int attendance = random.nextInt(2);
+                Random random  = new Random();
+                int attendance = random.nextInt(2);
+                int wagePerHour=20;
+                int fullDayHours=8;
+                int partTimeHours=4;
+                int wagePerDay=0;
 
-        int wagePerHour = 20;
-        int fullDayHours = 8;
-        int wagePerDay = wagePerHour + fullDayHours;
-
-        if (attendance == 1) {
-            System.out.println("Employee is present");
-            wagePerDay = wagePerHour * fullDayHours;
-        } else {
-            System.out.println("Employee is absent");
-            System.out.println("Employee wage Per Day : " + wagePerDay);
-
-
+                switch(attendance)
+                {
+                    case 0: System.out.println("Employee Absent");
+                        break;
+                    case 1: wagePerDay = partTimeHours * wagePerHour;
+                        break;
+                    default:
+                        wagePerDay =wagePerHour * fullDayHours;
+                }
+                if(attendance == 1) {
+                    System.out.println("Employee Present");
+                    wagePerDay = wagePerHour * fullDayHours;
+                }
+                else
+                    System.out.println("Employee Absent");
+                System.out.println("Employee Wage Per Day:" + wagePerDay);
+            }
         }
-    }
-}
